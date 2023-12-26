26869e5cd127f78eb0c6aebd6192b5f42c0c8fe7
Thalles <thallesrafaell@outlook.com>
2023-12-26 16:09:22 -0300

    Iniciando criaçào do checkout

---
+++ src/Pages/Checkout/index.tsx
@@ -0,0 +1,57 @@
+import Button from '../../components/Button'
+import Card from '../../components/Card'
+import { InputGroup, Row } from './styles'
+
+const Checkout = () => {
+  return (
+    <div className="container">
+      <>
+        <Card title="Dados de Cobrança">
+          <>
+            <Row>
+              <InputGroup>
+                <label htmlFor="fullName">Nome Completo</label>
+                <input id="fullName" type="text" />
+              </InputGroup>
+              <InputGroup>
+                <label htmlFor="email">E-mail</label>
+                <input id="email" type="email" />
+              </InputGroup>
+              <InputGroup>
+                <label htmlFor="cpf">CPF</label>
+                <input id="cpf" type="text" />
+              </InputGroup>
+            </Row>
+            <h3 className="margin-top">Dados de entrega - conteúdo digital</h3>
+            <Row>
+              <InputGroup>
+                <label htmlFor="deliveryEmail">E-mail</label>
+                <input id="deliveryEmail" type="email" />
+              </InputGroup>
+              <InputGroup>
+                <label htmlFor="confirmDeliveryEmail">Confirme o Email</label>
+                <input id="confirmDeliveryEmail" type="email" />
+              </InputGroup>
+            </Row>
+          </>
+        </Card>
+        <Card title="Pagamento">
+          <div>
+            <p>
+              Ao optar por essa forma de pagamento, é importante lembrar que a
+              confirmação pode levar até 3 dias úteis, devido aos prazos
+              estabelecidos pelas instituições financeiras. Portanto, a
+              liberação do código de ativação do jogo adquirido ocorrerá somente
+              após a aprovação do pagamento do boleto.
+            </p>
+          </div>
+        </Card>
+        <Button type="button" title="Clique aqui para finalizar a compra">
+          Finalizar a Compra
+        </Button>
+      </>
+    </div>
+  )
+}
+
+export default Checkout

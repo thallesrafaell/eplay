da530fa02c4371f7ccfbf4ff198fca0c7832bf78
Thalles <thallesrafaell@outlook.com>
2023-12-26 17:03:09 -0300

    Finalizando estilização do checkout

--- src/Pages/Checkout/index.tsx
+++ src/Pages/Checkout/index.tsx
@@ -1,8 +1,13 @@
+import { useState } from 'react'
 import Button from '../../components/Button'
 import Card from '../../components/Card'
-import { InputGroup, Row } from './styles'
+import { InputGroup, Row, TabButton } from './styles'
+import boleto from '../../assets/images/barcode.png'
+import cartao from '../../assets/images/credit-card.png'
 
 const Checkout = () => {
+  const [payWithCard, setPayWithCard] = useState(true)
+
   return (
     <div className="container">
       <>
@@ -19,7 +24,7 @@ const Checkout = () => {
               </InputGroup>
               <InputGroup>
                 <label htmlFor="cpf">CPF</label>
-                <input id="cpf" type="text" />
+                <input id="cpf" type="number" />
               </InputGroup>
             </Row>
             <h3 className="margin-top">Dados de entrega - conteúdo digital</h3>
@@ -36,15 +41,83 @@ const Checkout = () => {
           </>
         </Card>
         <Card title="Pagamento">
-          <div>
-            <p>
-              Ao optar por essa forma de pagamento, é importante lembrar que a
-              confirmação pode levar até 3 dias úteis, devido aos prazos
-              estabelecidos pelas instituições financeiras. Portanto, a
-              liberação do código de ativação do jogo adquirido ocorrerá somente
-              após a aprovação do pagamento do boleto.
-            </p>
-          </div>
+          <>
+            <TabButton
+              isActive={!payWithCard}
+              onClick={() => setPayWithCard(false)}
+            >
+              <img src={boleto} alt="Boleto Bancário" />
+              Boleto Bancário
+            </TabButton>
+            <TabButton
+              isActive={payWithCard}
+              onClick={() => setPayWithCard(true)}
+            >
+              <img src={cartao} alt="Cartão de crédito" />
+              Cartão de crédito
+            </TabButton>
+
+            <div className="margin-top">
+              {!payWithCard ? (
+                <p>
+                  Ao optar por essa forma de pagamento, é importante lembrar que
+                  a confirmação pode levar até 3 dias úteis, devido aos prazos
+                  estabelecidos pelas instituições financeiras. Portanto, a
+                  liberação do código de ativação do jogo adquirido ocorrerá
+                  somente após a aprovação do pagamento do boleto.
+                </p>
+              ) : (
+                <>
+                  <Row>
+                    <InputGroup>
+                      <label htmlFor="cardOwner">
+                        Nome do titular do cartão
+                      </label>
+                      <input type="text" id="cardOwner" />
+                    </InputGroup>
+                    <InputGroup>
+                      <label htmlFor="cpfCardOwner">
+                        CPF do Titular Cartão
+                      </label>
+                      <input id="cpfCardOwner" type="number" />
+                    </InputGroup>
+                  </Row>
+                  <Row marginTop="24px">
+                    <InputGroup>
+                      <label htmlFor="cardDisplayName">Nome no Cartão</label>
+                      <input type="text" id="cardDisplayName" />
+                    </InputGroup>
+                    <InputGroup>
+                      <label htmlFor="CardNumber">Número do Cartão</label>
+                      <input id="CardNumber" type="number" />
+                    </InputGroup>
+                    <InputGroup maxWidth="123px">
+                      <label htmlFor="expireMonth">Mes do Vencimento</label>
+                      <input id="expireMonth" type="number" />
+                    </InputGroup>
+                    <InputGroup maxWidth="123px">
+                      <label htmlFor="expireYear">Ano do vencimento</label>
+                      <input id="expireYear" type="number" />
+                    </InputGroup>
+                    <InputGroup maxWidth="48px">
+                      <label htmlFor="cardCode">CVV</label>
+                      <input id="cardCode" type="number" />
+                    </InputGroup>
+                  </Row>
+                  <Row marginTop="24px">
+                    <InputGroup maxWidth="158px">
+                      <label htmlFor="installments">Parcelamento</label>
+                      <select name="" id="">
+                        <option value="">1x de R$ 300,00</option>
+                        <option value="">2x de R$ 150,00</option>
+                        <option value="">3x de R$ 100,00</option>
+                      </select>
+                    </InputGroup>
+                  </Row>
+                </>
+              )}
+            </div>
+          </>
         </Card>
         <Button type="button" title="Clique aqui para finalizar a compra">
           Finalizar a Compra
--- src/Pages/Checkout/styles.ts
+++ src/Pages/Checkout/styles.ts
@@ -1,24 +1,57 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
 
-export const Row = styled.div`
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type RowProps = {
+  marginTop?: string
+}
+
+type TabButtonProps = {
+  isActive: boolean
+}
+
+export const Row = styled.div<RowProps>`
   display: flex;
   column-gap: 24px;
+  margin-top: ${(props) => props.marginTop || 'auto'};
+  align-items: flex-end;
 `
 
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
+  max-width: ${(props) => props.maxWidth || 'auto'};
   label {
     font-size: 14px;
     margin-bottom: 8px;
     display: block;
   }
 
-  input {
+  input,
+  select {
     background-color: ${cores.branca};
     height: 32px;
     padding: 0 8px;
     border: 1px solid ${cores.branca};
     width: 100%;
+  }
+`
+
+export const TabButton = styled.button<TabButtonProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${cores.branca};
+  background-color: ${(props) => (props.isActive ? cores.verde : cores.preta)};
+  height: 32px;
+  border: none;
+  margin-right: 16px;
+  padding: 0 8px;
+  cursor: pointer;
+
+  img {
+    margin-right: 8px;
   }
 `
